c1a49217de95dafc94666db3e221e3df3b206af3
Chris Troutner <chris.troutner@gmail.com>
2022-11-30 12:50:13 -0800

    Adding BUX token

--- src/components/offers/verified.js
+++ src/components/offers/verified.js
@@ -6,6 +6,7 @@ class VerifiedTokens {
   constructor () {
     this.tetherId = '9fc89d6b7d5be2eac0b3787c5b8236bca5de641b5bafafc8f450727b63615c11'
     this.psfId = '73fcc96ad73948a102a8cabac8fe1808f94f1a8ec5536b0f4d7c524ac63fc44a'
+    this.bux = '7e7dacd72dcdb14e00a03dd3aff47f019ed51a6f1f4e4f532ae50692f62bc4e5'
   }
 
   // Check the Ticker against verified Token IDs. This will return true if the
@@ -24,6 +25,15 @@ class VerifiedTokens {
     // PSF
     if (ticker.includes('PSF')) {
       if (tokenId === this.psfId) {
+        return false
+      } else {
+        return true
+      }
+    }
+
+    // BUX
+    if (ticker.includes('BUX')) {
+      if (tokenId === this.bux) {
         return false
       } else {
         return true
